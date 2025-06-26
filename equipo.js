560d4875ee71189c78f03a7c05e553b4e763c0c6
REYESSOFT <ricardo.reyes83.drj@gmail.com>
2025-06-25 20:24:16 -0500

    Add files via upload

--- equipo.js
+++ equipo.js
@@ -1,4 +1,3 @@
-
 function mostrarInfo(persona) {
   const info = {
     daniel: `
@@ -8,6 +7,10 @@ function mostrarInfo(persona) {
     veronica: `
       <h3>Verónica Valle Astudillo</h3>
       <p>Economista, Magíster en Tributación y Finanzas, con Diplomado en Tributación. Más de 15 años de experiencia asesorando en áreas contables, tributarias, societarias y financieras. Dirige el área de Outsourcing Contable y Asesorías Tributarias en REYVAL para diversos sectores económicos.</p>
+    `,
+    andrea: `
+      <h3>Andrea</h3>
+      <p>Soy tecnóloga Superior en Contabilidad, profesional con experiencia como asistente contable y de auditoría, brindando soporte en el registro de operaciones financieras, control de documentación contable, conciliaciones bancarias, análisis de cuentas y preparación de informes para procesos de auditoría interna y externa. Cuento con habilidades en la revisión de registros contables conforme a normativas vigentes y políticas internas, así como en la detección de inconsistencias o riesgos financieros. Mi aporte en la firma es trabajar de forma organizada, analítica y discreta, y colaborar activamente en el equipo contable y de auditoría para garantizar la transparencia y confiabilidad de la información financiera de los clientes.</p>
     `
   };
   document.getElementById("info-equipo").innerHTML = info[persona];
